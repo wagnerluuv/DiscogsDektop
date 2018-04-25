9a1cb301967d7344701f9a28a3b543b056bfd956
wagnerluuv <felixwagner90@gmx.de>
2018-04-25 23:29:41 +0200

    Added libDiscogsDesktop as Buisness-Logic-Library
    the place where all the magic happens (youtube downloading, converting etc...)

---
+++ DiscogsDesktop/libDiscogsDesktop/Services/ConverterService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaToolkit;
+using MediaToolkit.Model;
+
+namespace libDiscogsDesktop.Services
+{
+    public static class ConverterService
+    {
+        private const string Folder = "D:\\LIBRARY\\Downloads";
+
+        public static void ConvertVideoToMp3(string filepath)
+        {
+            MediaFile inputFile = new MediaFile { Filename = filepath };
+            MediaFile outputFile = new MediaFile { Filename = $"{filepath}.mp3" };
+
+            using (Engine engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
+        }
+    }
+}
---
+++ DiscogsDesktop/libDiscogsDesktop/Services/YoutubeService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using VideoLibrary;
+using YoutubeExtractor;
+
+namespace libDiscogsDesktop.Services
+{
+    public static class YoutubeService
+    {
+        private const string Folder = "D:\\LIBRARY\\Downloads";
+
+        public static void DownloadVideo(string url, out string filepath)
+        {
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(url);
+
+            VideoInfo video = videoInfos
+                .First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+            
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            filepath = Path.Combine(Folder, "With Youtube Extractor" + video.VideoExtension);
+
+            VideoDownloader videoDownloader = new VideoDownloader(video, filepath);
+            
+            videoDownloader.Execute();
+        }
+   
+        public static void DownloadVideo2(string url, out string filepath)
+        {
+            YouTube youTube = YouTube.Default; // starting point for YouTube actions
+            YouTubeVideo video = youTube.GetVideo(url); // gets a Video object with info about the video
+            filepath = Path.Combine(Folder, "With Video Library" + video.FileExtension);
+            File.WriteAllBytes(filepath, video.GetBytes());
+        }
+    }
+}
